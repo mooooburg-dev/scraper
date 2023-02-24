43d5fa523892763ac3f64f1ad429e25340063dfa
mooooburg-dev <jeongmupark@gmail.com>
2023-02-24 18:42:32 +0900

    🔨 scrap-test 페이지 생성

---
+++ pages/scrap-test/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+type Props = {
+  searchKeyword: string;
+};
+
+const baseURL: string = 'https://www.coupang.com';
+
+function ProductsPage({ searchKeyword }: Props) {
+  return <div className="container px-32">{searchKeyword}</div>;
+}
+
+const axios = require('axios');
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const { query } = context;
+
+  const searchId: string = query.q;
+
+  const response = await axios.get(
+    // `https://www.coupang.com/np/search?rocketAll=false&q=${searchId}&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=auto&backgroundColor=&searchProductCount=122651&component=&rating=0&sorter=scoreDesc&listSize=36`,
+    `https://www.cgv.co.kr/`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
+      },
+    }
+  );
+
+  return {
+    props: {
+      searchKeyword: searchId,
+    },
+  };
+};
+
+export default ProductsPage;
