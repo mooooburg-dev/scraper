e00bfecd55253f3a2fc625bcf3ded77240d693f2
mooooburg-dev <jeongmupark@gmail.com>
2023-09-25 22:02:08 +0900

    User-Agent

--- pages/scrap-axios/index.tsx
+++ pages/scrap-axios/index.tsx
@@ -49,6 +49,8 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
       headers: {
         'Content-Type': 'application/json',
         'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
       },
     }
   );
